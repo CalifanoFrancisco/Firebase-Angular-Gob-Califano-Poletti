5c4e86caf972fcbb6e972034e7d21a27732d2637
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-08-07 16:08:59 -0300

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'api-1h-pruscio',
+    appId: '1:799849807668:web:6205b9719c6e765f6caf61',
+    storageBucket: 'api-1h-pruscio.appspot.com',
+    apiKey: 'AIzaSyAeueiFFJrIofZqhONmetx2TXx1HlDv1aU',
+    authDomain: 'api-1h-pruscio.firebaseapp.com',
+    messagingSenderId: '799849807668',
+    measurementId: 'G-ME3W0FTHW0',
+  },
+};
